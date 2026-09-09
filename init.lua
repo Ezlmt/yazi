5bb06a9dbc59bddb1053937810a35655c57c25e5
Ezlmt <2876187491@qq.com>
2026-09-09 17:33:26 +0000

    feat: auto update zoxide database on cd

--- init.lua
+++ init.lua
@@ -2,5 +2,7 @@ require("starship"):setup({
 	config_file = "~/.config/yazi/starship.toml",
 })
 require("full-border"):setup()
-
 require("git"):setup()
+require("zoxide"):setup({
+	update_db = true,
+})
